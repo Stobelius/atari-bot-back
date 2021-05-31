b25a9b6006bbf75e884146dcb14c9ca48db209fb
Jouko Kelomäki <jouko.kelomaki@gmail.com>
2021-05-31 17:02:15 +0300

    enable cors

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask,request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/test", methods=['POST'])
 def hello_world():
     data=json.loads(request.data)
     print(data["test"])
     newdata=makeMove(data["board"])
-    return {"board": newdata} 
+    return jsonify({"board": newdata})
 
 def makeMove(board):
     newboard=board
